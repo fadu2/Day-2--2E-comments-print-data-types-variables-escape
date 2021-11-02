d3391d81484525b176fc0701fd66199f1ebabe61
FrancisOhemengA <93487638+fadu2@users.noreply.github.com>
2021-11-02 19:47:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,81 @@
+# for single line comment  
+# asdfadsfasd 
+# adfsadasdf
+
+"""
+This for multi-line comments
+asdfadsfasd
+asdfasdfadsfasdf
+
+afdadsfasdfasdfs
+
+asdfsadfsadf
+"""
+
+# print output
+print('hello')
+print("hello")
+# print 456 times 3
+print(456 * 3)
+# print 83 divided by 3 
+print(83 / 3)
+# print 786 minus 890
+print(786 - 860)
+
+# + add
+# - subtract 
+# * multiply 
+# / divide 
+
+# Data types 
+# integer - 5, 2, 10, 1000, 420
+# float - 3.14, 45.1, 10.0, 100.001 
+# string - "hello", "3.14", "I am", 'a'
+# boolean - True, False  
+
+#1 78.8 - float 
+#2 "false" - string 
+#3 "5.23"  - string 
+#4 false - error 
+#5 False - boolean  
+
+# Variables 
+# Name given to some data in memory 
+age = 15 
+print(age)
+# hours, age, rate, pay 
+age = 17 
+print(age)
+
+# Rules for variables names
+#1) names must start with a letter (preferably lower case)
+#2) names should not start with a number. 
+#3) names can be alphanumeric (letters and numbers)
+#4) names should not have special characters, except _ underscore 
+#5 No python keyword or reserved word 
+
+number_of_students = 14  # underscores 
+numberOfStudents = 24  # camelcase 
+#number of students = 23 
+"""
+7Age  
+Age7 
+age7 
+age 7 = 14 
+#age = 67 
+age# = 65 
+"""
+
+# Escape characters 
+# \n -- new line 
+print("hello\nworld")
+
+# \t -- tab space 
+print("hello\tworld")
+
+# \\ -- prints \ 
+print("\\")
+print("\\\\")
+print(" \" ")
+print("He said \"hello\" ")
+
